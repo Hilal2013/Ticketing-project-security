8409eede3c45474d56b1f5b1658062da2f2d23f1
Hilal2013 <hilalozkul@gmail.com>
2023-05-13 20:17:22 -0400

    SecurityServiceImpl

---
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -0,0 +1,25 @@
+package com.cydeo.service.impl;
+
+import com.cydeo.entity.User;
+import com.cydeo.repository.UserRepository;
+import com.cydeo.service.SecurityService;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+@Service
+public class SecurityServiceImpl implements SecurityService {
+    private final UserRepository userRepository;
+
+    public SecurityServiceImpl(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        //get the user from db (this is business->go to repository) and convert to user springs understands by using UserPrincipal
+User user=userRepository.findByUserNameAndIsDeleted(username,false);
+//I need to authenticate this user
+        return null;
+    }
+}
