fc560b5d7f7c2154af1d94d5d70acf74e99b34fa
Hilal2013 <hilalozkul@gmail.com>
2023-05-14 22:27:30 -0400

    updated

--- src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
+++ src/main/java/com/cydeo/service/impl/SecurityServiceImpl.java
@@ -1,7 +1,7 @@
 package com.cydeo.service.impl;
 
 import com.cydeo.entity.User;
-import com.cydeo.entity.common.UserPrinciple;
+import com.cydeo.entity.common.UserPrincipal;
 import com.cydeo.repository.UserRepository;
 import com.cydeo.service.SecurityService;
 import org.springframework.security.core.userdetails.UserDetails;
@@ -25,6 +25,6 @@ User user=userRepository.findByUserNameAndIsDeleted(username,false);
             throw new UsernameNotFoundException(username);
         }
 
-        return new UserPrinciple(user);//returning object so wwe need constructor in UserPrincipleClass
+        return new UserPrincipal(user);//returning object so wwe need constructor in UserPrincipleClass
     }
 }
