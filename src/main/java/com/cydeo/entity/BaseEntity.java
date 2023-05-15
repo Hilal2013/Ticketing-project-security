d00a4582565675e23ff952571994b98a4f430e70
Hilal2013 <hilalozkul@gmail.com>
2023-05-14 22:27:35 -0400

    updated

--- src/main/java/com/cydeo/entity/BaseEntity.java
+++ src/main/java/com/cydeo/entity/BaseEntity.java
@@ -30,24 +30,6 @@ public class BaseEntity {
     private Long lastUpdateUserId;
 
     //this method needs to be executed whenever we create the object=>a new user/project/manager
-    @PrePersist//trying save//
-    private void onPrePersist(){
-        //I created  a user is gonna current time
-        this.insertDateTime = LocalDateTime.now();
-        //in the beginning is gonna be current time too
-        this.lastUpdateDateTime=LocalDateTime.now();
-        this.insertUserId=1L;
-        //security is not connected yet//this is now hard code
-        //when we start the security this will be connected with security
-        this.lastUpdateUserId=1L;
-    }
-    //this method executed whenever we update the object
-    @PreUpdate
-    private void onPreUpdate(){
-        //is gonna be update
-        this.lastUpdateDateTime=LocalDateTime.now();
-        //whoever is updating this user//is gonna take that primary key
-        this.lastUpdateUserId=1L;
-    }
+
 
 }
